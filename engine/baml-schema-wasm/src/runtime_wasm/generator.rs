cdc1838c114fd4d13e032da9cf7312aa55a8a889
aaronvg <aaron@boundaryml.com>
2024-12-15 19:47:20 -0800

    Fix windows generation through vscode always outputting to the root workspace directory if the path was ../ (#1247)
    The issue for windows is that `g.output_dir` is always /baml_client when
    the user set it to `../`. Rather than have rust try to do fancy things
    like resolve the output directory (which doesn't seem to work in wasm in
    windows), give VSCode the relative path to baml_src, since vscode can
    build it from the project.path
    
    <!-- ELLIPSIS_HIDDEN -->
    
    
    > [!IMPORTANT]
    > Fixes file generation path issue in `baml_project_manager.ts` and
    updates `dunce` package version in `Cargo.lock`.
    > 
    >   - **Behavior**:
    > - Fixes file generation path issue in `baml_project_manager.ts` by
    using `output_dir_relative_to_baml_src` for output directory.
    >     - Logs output directory paths for debugging.
    >   - **Dependencies**:
    > - Updates `dunce` package version from `1.0.4` to `1.0.5` in
    `Cargo.lock`.
    > 
    > <sup>This description was created by </sup>[<img alt="Ellipsis"
    src="https://img.shields.io/badge/Ellipsis-blue?color=175173">](https://www.ellipsis.dev?ref=BoundaryML%2Fbaml&utm_source=github&utm_medium=referral)<sup>
    for 594a4cd45d95f3a32f7abeb12278fd09a7be7774. It will automatically
    update as commits are pushed.</sup>
    
    
    <!-- ELLIPSIS_HIDDEN -->

--- engine/baml-schema-wasm/src/runtime_wasm/generator.rs
+++ engine/baml-schema-wasm/src/runtime_wasm/generator.rs
@@ -6,6 +6,8 @@ pub struct WasmGeneratorOutput {
     #[wasm_bindgen(readonly)]
     pub output_dir: String,
     #[wasm_bindgen(readonly)]
+    pub output_dir_relative_to_baml_src: String,
+    #[wasm_bindgen(readonly)]
     pub files: Vec<WasmGeneratedFile>,
 }
 
@@ -22,6 +24,10 @@ impl Into<WasmGeneratorOutput> for GenerateOutput {
     fn into(self) -> WasmGeneratorOutput {
         WasmGeneratorOutput {
             output_dir: self.output_dir_full.to_string_lossy().to_string(),
+            output_dir_relative_to_baml_src: self
+                .output_dir_shorthand
+                .to_string_lossy()
+                .to_string(),
             files: self
                 .files
                 .into_iter()
